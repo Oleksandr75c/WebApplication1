d9a1de1faa2fcb26b13349cf6ef50bae6989db5b
study_75@outlook.com <study_75@outlook.com>
2023-08-08 20:36:58 +0300

    hw6

---
+++ HW5/Pages/Homework6.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW5.Pages
+{
+    public class Homework6Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
